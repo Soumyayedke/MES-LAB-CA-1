4b4a04e24d5d30b72c9f8b23fc6aa65757208526
Soumya Yedke <soumyayedke@gmail.com>
2026-02-04 23:54:18 +0530

    Create Case Study Q2 Case 2.asm

---
+++ Case Study Q2 Case 2.asm
@@ -0,0 +1,44 @@
+ORG 0000H
+
+; -------- Test values (change for cases) --------
+MOV 50H,#05H;
+MOV 51H,#05H;
+; -----------------------------------------------
+
+; Copy to registers
+MOV R0,50H
+MOV R1,51H
+
+COMPARE:
+
+MOV A,R0
+JZ A_ZERO
+
+MOV A,R1
+JZ B_ZERO
+
+DEC R0
+DEC R1
+SJMP COMPARE
+
+; ------------ A reached zero first ------------
+A_ZERO:
+MOV A,R1
+JZ EQUAL
+
+MOV R7,#0FFH     ; A < B
+SJMP DONE
+
+; ------------ B reached zero first ------------
+B_ZERO:
+MOV R7,#01H      ; A > B
+SJMP DONE
+
+; ------------ Equal ------------
+EQUAL:
+MOV R7,#00H
+
+DONE:
+SJMP DONE
+
+END
